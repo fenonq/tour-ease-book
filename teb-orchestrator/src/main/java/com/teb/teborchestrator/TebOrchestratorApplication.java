30068e900eb090272fa9ccf846bbfbd2bd6ef7ca
fenonq <fenyk.ttt@gmail.com>
2024-01-26 00:43:18 +0200

    create new services

---
+++ teb-orchestrator/src/main/java/com/teb/teborchestrator/TebOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.teb.teborchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TebOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TebOrchestratorApplication.class, args);
+    }
+
+}
---
+++ teb-orchestrator/src/test/java/com/teb/teborchestrator/TebOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teb.teborchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TebOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
